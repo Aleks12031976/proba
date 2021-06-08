f320312dd14bff7d3d8a20929412d60e42c700b0
wsruser1 <alekseev_lnt@mail.ru>
2021-06-08 10:53:57 +0300

    Initial Commit

---
+++ First/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First
+//
+//  Created by user on 08.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by user on 08.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
